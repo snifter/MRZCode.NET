7fd51737aae91c5835ccc6310e3ac7f30eae2e09
Sergiu <s@primefin.tech>
2023-06-16 13:35:23 +0300

    Same case for TD2. Expiration Date for ID can be unlimited.

--- MRZCodeParser/CodeTypes/TD2SecondLine.cs
+++ MRZCodeParser/CodeTypes/TD2SecondLine.cs
@@ -9,7 +9,7 @@ namespace MRZCodeParser.CodeTypes
         }
 
         protected override string Pattern =>
-            "([A-Z0-9<]{9})([0-9]{1})([A-Z<]{3})([0-9]{6})([0-9]{1})([M|F|X|<]{1})([0-9]{6})([0-9]{1})([A-Z0-9<]{7})([0-9]{1})";
+            "([A-Z0-9<]{9})([0-9]{1})([A-Z<]{3})([0-9]{6})([0-9]{1})([M|F|X|<]{1})([0-9<]{6})([0-9]{1})([A-Z0-9<]{7})([0-9]{1})";
 
         internal override IEnumerable<FieldType> FieldTypes => new[]
         {
